a43efb6f5f9da4e3037ccd05b1ec07c2551d2be3
Arjun <arjunp1601@gmail.com>
2025-01-18 16:34:37 +0530

    login component

--- todo-ui/src/components/LoginComponent.jsx
+++ todo-ui/src/components/LoginComponent.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { logjnAPICall } from "../services/AuthService";
+import { useNavigate } from "react-router-dom";
 
 const LoginComponent = () => {
-  return <div>LoginComponent</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigator = useNavigate();
+
+  function handleLoginForm(e) {
+    e.preventDefault();
+    const loginObj = { username, password };
+    console.log(loginObj);
+    logjnAPICall(username, password)
+      .then((response) => {
+        console.log(response);
+        navigator("/todos");
+      })
+      .then((error) => console.log(error));
+  }
+
+  return (
+    <div className="container">
+      <br />
+      <br />
+      <div className="row">
+        <div className="card col-md-6 offset-md-3">
+          <div className="card-header">
+            <h2 className="text-center">Login Form</h2>
+          </div>
+          <div className="card-body">
+            <form>
+              <div className="row mb-3">
+                <label className="col-md-3 control-label">
+                  Username or Email
+                </label>
+                <div className="col-md-9">
+                  <input
+                    type="text"
+                    name="username"
+                    className="col-md-9"
+                    placeholder="Enter Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  ></input>
+                </div>
+              </div>
+              <div className="row mb-3">
+                <label className="col-md-3 control-label">Password</label>
+                <div className="col-md-9">
+                  <input
+                    type="text"
+                    name="password"
+                    className="col-md-9"
+                    placeholder="Enter Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></input>
+                </div>
+              </div>
+              <div className="form-group mb-3">
+                <button
+                  className="btn btn-primary"
+                  onClick={(e) => handleLoginForm(e)}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginComponent;
--- todo-ui/src/services/AuthService.js
+++ todo-ui/src/services/AuthService.js
@@ -4,3 +4,6 @@ const AUTH_REST_API_BASE_URL = "http://localhost:8080/api/auth";
 
 export const registerAPICall = (registerObj) =>
   axios.post(AUTH_REST_API_BASE_URL + "/register", registerObj);
+
+export const logjnAPICall = (usernameOrEmail, password) =>
+  axios.post(AUTH_REST_API_BASE_URL + "/login", { usernameOrEmail, password });
